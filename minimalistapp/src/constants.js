66f5d954d9613788699325b75a686f298cddeda1
Manomay Kagalkar <manomay111@gmail.com>
2023-01-02 00:31:35 +0530

    feature three ready

--- minimalistapp/src/constants.js
+++ minimalistapp/src/constants.js
@@ -7,3 +7,46 @@ export const navItemsForHome = [
     {link: '#footer', text: 'About'},
     {link: '#public-home', text: 'Top'},
 ];
+export const professions = [
+    {text: 'collaboration tool for everyone', emoji: '👩🏽 👨🏻 🧑🏻 🧔🏾‍♂️ 👩🏻'},
+    {text: 'collaboration tool for developers', emoji: '👩🏽‍💻 👨🏻‍💻 🧑🏻‍💻 👨🏾‍💻 👩🏻‍💻'},
+    {text: 'collaboration tool for students', emoji: '👩🏽‍🎓 👨🏻‍🎓 🧑🏻‍🎓 👨🏽‍🎓 👩🏻‍🎓'},
+    {text: 'collaboration tool for lawyers', emoji: '👩🏽‍💼 👨🏻‍💼 🧑🏻‍💼 👨🏾‍💼 👩🏻‍💼'},
+    {text: 'collaboration tool for doctors', emoji: '👩🏽‍⚕️ 👨🏻‍⚕️ 🧑🏻‍⚕️ 👨🏾‍⚕️ 👩🏻‍⚕️'},
+    {text: 'yes, chefs can use it too!', emoji: '👩🏽‍🍳 👨🏻‍🍳 🧑🏻‍🍳 👨🏽‍🍳 👩🏻‍🍳'},
+]
+
+export class professionsList {
+    constructor() {
+        this.head = {
+            data: professions[0],
+            next: null,
+        }
+        this.one = {
+            data: professions[1],
+            next: null,
+        }
+        this.two = {
+            data: professions[2],
+            next: null,
+        }
+        this.three = {
+            data: professions[3],
+            next: null,
+        }
+        this.four = {
+            data: professions[4],
+            next: null,
+        }
+        this.five = {
+            data: professions[5],
+            next: null,
+        }
+        this.head.next = this.one;
+        this.one.next = this.two;
+        this.two.next = this.three;
+        this.three.next = this.four;
+        this.four.next = this.five;
+        this.five.next = this.head;
+    }
+}
--- minimalistapp/src/pages/Home.jsx
+++ minimalistapp/src/pages/Home.jsx
@@ -1,8 +1,27 @@
 import { Link } from 'react-router-dom';
 import './Home.css';
 import teamnamesOne from '../resources/teamnames-one.png';
+import paneThreeImage from '../resources/task-division.png';
+import { professions, professionsList } from '../constants';
+import { useState, useEffect } from 'react';
 
 const Home = ({ isLightTheme }) => {
+
+    useEffect(() => {
+        const text = document.getElementById('profession-text');
+        const emoji = document.getElementById('profession-emoji');
+        let list = new professionsList();
+        let node = list.head;
+        const interval = setInterval(() => {
+            node = node.next;
+            text.textContent = node.data.text;
+            emoji.textContent = node.data.emoji;
+        }, 1500);
+        return(() => {
+            clearInterval(interval);
+        });
+    })
+
     return(
         <div id='public-home'>
             <div className='home-spacer'></div>
@@ -26,17 +45,20 @@ const Home = ({ isLightTheme }) => {
                     <span className='feature-title'>You get a quirky team code<br></br>\(• ◡ •)/</span>
                 </div>
                 <div id='feature-pane-two' className='featurePane'>
-                    <span className='feature-title'>🚧 🦺 work in progress 🦺 🚧</span>
+                <span id='profession-text'>collaboration tool for everyone</span>
+                    <h1 id='profession-emoji'>👩🏾‍🦱 👨🏻 👩🏽 🧔🏾‍♂️ 👩🏻</h1>
+                    <span className='feature-title'>The code adds them to the team<br></br>(‘-’)人(ﾟ_ﾟ)</span>
                 </div>
                 <div id='feature-pane-three' className='featurePane'>
-                    <span className='feature-title'>🚧 🦺 work in progress 🦺 🚧</span>
+                    <img className='paneThree-image' src={paneThreeImage} alt="task distribution" />
+                    <span className='feature-title'>Pick tasks from the team board 📌</span>
                 </div>
                 <div id='feature-pane-four' className='featurePane'>
                     <span className='feature-title'>🚧 🦺 work in progress 🦺 🚧</span>
                 </div>
             </div>
-            <div id='footer'>
-                <div style={{height: '300px'}}><h1>Footer</h1></div>
+            <div id='footer' className={isLightTheme? ' light': ' dark'}>
+
             </div>
         </div>
     )
